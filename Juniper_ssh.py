9e38cb3876508ff83756268a960b6013be8229d8
Shamen Snyder <shamen.snyder@gmail.com>
2016-12-16 21:00:14 -0500

    Update Juniper_ssh.py

--- Juniper_ssh.py
+++ Juniper_ssh.py
@@ -7,24 +7,6 @@ import re
 import os
 
 print('CTRL-C to Exit Program at any time.')
-time.sleep(1)
-
-while True:
-    host_file_or_ip_address = input('Do you want to use a host file?(Enter yes or no):')
-    if host_file_or_ip_address == 'yes':
-        file_path = input('Enter file path for host_file: ')
-        # Opens file in read mode
-        f1 = open(file_path,'r')
-        # Creates list
-        devices = f1.readlines()
-        print(devices)
-    elif host_file_or_ip_address == 'no':
-        devices = input('Enter IP Addresses:')
-        print(devices)
-    else:
-        print('Enter yes or no:')
-
-username = input('Enter username for device login:')
 
 def enterPassword():
   while True: # repeat forever
@@ -34,7 +16,36 @@ def enterPassword():
       print ('Passwords do not match. Please try again.')
     else:
       return password
+
+def getDevices():
+  while True:
+    host_file_or_ip_address = input('Do you want to use a host file?(Enter yes or no):')
+    if host_file_or_ip_address == 'yes':
+      file_path = input('Enter file path for host_file: ')
+      with open(file_path,'r') as f1: # Opens file in read mode
+        devices = f1.read().splitlines() # Creates list removes \n
+        f1.close()
+      return devices
+    elif host_file_or_ip_address == 'no':
+      devices = []
+      i = 0
+      while 1:
+        i += 1
+        device = input('Enter IP Addresses (Press Enter to stop):')
+        if device == '':
+          break
+        devices.append(device)
+        devices = [x.strip(' ') for x in devices]
+        print(devices)
+        return devices
+    else:
+        print('Enter yes or no:')
+
+username = input('Enter username for device login:')
+
 password = enterPassword()
+
+devices = getDevices()
 
 run_command = input('Type command you would like to run:')
 command_not_allowed = re.match(r'\brequest\b|\brestart\b|\bstart\b', run_command) #Regular Expression
@@ -53,6 +64,8 @@ for device in devices:
     print(output)
     print("*** END ***\n")
 
+
 end_time = datetime.now()
 total_time = end_time - start_time
 print ('Total time to run script: ', total_time)
+
